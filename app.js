b07a602d6686725a3e4c9a8ff81473ec195242c6
pjesek <business.pjesek@gmail.com>
2023-09-06 21:06:05 +0200

    init

---
+++ app.js
@@ -0,0 +1,64 @@
+const { io } = require('socket.io-client');
+const osc = require('./osc');
+
+const config = require('./config.json');
+const socket = io(`https://sockets.streamlabs.com?token=${config.streamlabs_socket_token}`);
+
+const address = "localhost";
+const port = 9000;
+
+socket.on("connect", () => {
+    console.log("Connected to Streamlabs, Listening for alerts ^‿^");
+});
+  
+socket.on("disconnect", () => {
+    console.log("Connection with Streamlabs disconnected.");
+});
+
+socket.on("event", (eventData) => {
+    if(eventData.for === "twitch_account") {
+        switch(eventData.type) {
+            case 'follow':
+                console.log("New follower!");
+                osc.sendFloat({
+                    path: config.follower_osc_parameter,
+                    data: 1
+                    });
+                delayInterval = setTimeout(function() {
+                    osc.sendFloat({
+                        path: config.follower_osc_parameter,
+                        data: 0
+                        });
+                    console.log("back to default value");
+                }, 1000);
+                break;
+            case 'subscription':
+                console.log("New subscriber!")
+                osc.sendFloat({
+                    path: config.subscriber_osc_parameter,
+                    data: 1
+                    });
+                delayInterval = setTimeout(function() {
+                    osc.sendFloat({
+                        path: config.subscriber_osc_parameter,
+                        data: 0
+                        });
+                    console.log("back to default value");
+                }, 1000);
+                break;
+        }
+    }
+})
+
+osc.init({
+    address,
+    port,
+    onReady: () => {
+      console.log(`OSC Client listening to "${address}:${port}"`);
+    },
+    onError: (err) => {
+      console.log(`The OSC driver experienced an error! ${err}`);
+      process.exit();
+    }
+  });
+
---
+++ osc.js
@@ -0,0 +1,47 @@
+const osc = require("osc");
+
+let udpPort = null;
+
+const init = ({
+  addr,
+  port,
+  onReady,
+  onError
+}) => {
+  
+  udpPort = new osc.UDPPort({
+    localAddress: "localhost",
+    localPort: port+1,
+    remoteAddress: addr,
+    remotePort: port,
+    broadcast: true
+  });
+  
+  udpPort.on("ready", onReady);
+  udpPort.on("error", onError);
+  
+  udpPort.open();
+};
+
+const sendFloat = ({
+  path,
+  data
+}) => {
+  if (udpPort == null) {
+    throw new Error("OSC client not initialized!");
+  }
+  udpPort.send({
+    address: path,
+    args: [
+      {
+        type: "f",
+        value: data
+      }
+    ]
+  });
+}
+
+module.exports = {
+  init,
+  sendFloat
+}
